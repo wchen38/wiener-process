a7a99361fb7cdffb384f5ed9d6500bad62377316
Wuyuan <wuyuan@pureengineering.com>
2018-04-30 12:48:45 -0700

    got the process from t1 to t2

--- hw3/matlab/hw3_part2a.m
+++ hw3/matlab/hw3_part2a.m
@@ -21,44 +21,39 @@ expectW(index) = 0;
 varW(1) = 1;
 varW(index) = 1;
 
-%calcuates the expected value from t1 to t2
-%each expected value is stored in expectW(k)
 
+
+for b=1:1:100
     k=1;
+    %calcuates the expected value from t1 to t2
+    %each expected value is stored in expectW(k)
     for t=1.05:0.05:n
-        k = k+1;
         t_prev=t-delta_t;
-        A = ( (t2-t)/(t2-t_prev) )*w(k);
-        B = ( (t-t_prev)/(t2-t_prev) )*w(index);
-        expectW(k) = A+B;
         
-    end
-    
-   % plot(expectW);
-   % hold on
-
-%calcuates the variance from t1 to t2
-%each variance value is stored in varW(k)
-    k=1;
-    for t=1.05:0.05:n
-        k = k+1;
-        t_prev=t-delta_t;
-        A = (t2-t) * (t2-t_prev);
+        %expected value
+        A = ( (t2-t)/(t2-t_prev) )*x(k);
+        B = ( (t-t_prev)/(t2-t_prev) )*x(index);
+        expectW(k+1) = A+B;
+        
+        %variance
+        A = (t2-t) * (t-t_prev);
         B = t2-t_prev;
-        varW(k) = A/B;
+        varW(k+1) = A/B;
+        
+        %x(k)
+        x(k+1) = (rand*varW(k+1)) + expectW(k+1);
+        k = k+1;
         
     end
-    %plot(varW);
     
-%calculate x(k)
-
-k=1;
-for t=2:1:index-1
-    rand_val = expectW(k) + (rand*sqrt(varW(k)));
-    x(t) = x(t-1) + rand_val;
-    k =k+1;
+    plot(x);
+    hold on
+    
+    
+    
+    
 end
 
-plot(x);
+
 
 
