28ffca6411c4927f731c4e3854e61c523daa11d4
Wuyuan <wuyuan@pureengineering.com>
2018-04-30 13:10:15 -0700

    got t2 to t3

--- hw3/matlab/hw3_part2a.m
+++ hw3/matlab/hw3_part2a.m
@@ -1,28 +1,30 @@
 expectW= [];
 varW = [];
-delta_t = 0.05;
+delta_t = 0.05*2;
 t1 = 1;
 t2 = 2;
-t3 = -1;
+t3 = 3;
 %when t equal to 2, the index will be 22
 index = 21;
+t3_index = 41;
 x(1) = 1;
 x(index) = 2;
-n = 2-0.05;
-%wiener process of w1 and w2 is just a normal distribution
-w(1) = rand*sqrt(1);
-w(index)= rand*sqrt(2-1);
+x(t3_index) = -1;
+n = t2-0.05;
+m = t3-0.05;
+
 
 %expected value of given variables
 expectW(1) = 0;
 expectW(index) = 0;
-
+expectW(t3_index) = 0;
 %variance of given variables
 varW(1) = 1;
 varW(index) = 1;
+varW(t3_index) = 1;
 
 
-
+%run the sequence 100 times
 for b=1:1:100
     k=1;
     %calcuates the expected value from t1 to t2
@@ -46,11 +48,30 @@ for b=1:1:100
         
     end
     
+    %calcuates the expected value from t2 to t3
+    %each expected value is stored in expectW(k)
+    k=index;
+    for t=2.05:0.05:m
+        
+        t_prev=t-delta_t;
+        
+        %expected value
+        A = ( (t3-t)/(t3-t_prev) )*x(k);
+        B = ( (t-t_prev)/(t3-t_prev) )*x(t3_index);
+        expectW(k+1) = A+B;
+        
+        %variance
+        A = (t3-t) * (t-t_prev);
+        B = t3-t_prev;
+        varW(k+1) = A/B;
+        
+        %x(k)
+        x(k+1) = (rand*varW(k+1)) + expectW(k+1);
+        k = k+1;
+        
+    end
     plot(x);
     hold on
-    
-    
-    
     
 end
 
