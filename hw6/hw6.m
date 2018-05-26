a5d05be168620732b2fb447492261831d3befb5e
Wuyuan <wuyuan@pureengineering.com>
2018-05-26 11:28:22 -0700

    added comments on the prediction step

--- hw6/hw6.m
+++ hw6/hw6.m
@@ -5,9 +5,12 @@ clc
 %Wuyuan Chen
 %
 init_value = [10000 -500 6*(10^7) 50 0 0 0 200 0 0 0 2*(10^12)];
-%init_value = [10000; -500; 6*(10^7); 50; 0; 0; 200; 0; 2*(10^12)];
-%init_value = [1 1];
-[t, x] = ode45('myfunc', [0 1], init_value);
-t = t';
-x = x';
+for k=1:1:10
+    %using ode45 to solve for x_dot_hat(t) and A(t) in the prediction step 
+    [t, x] = ode45('myfunc', [0 1], init_value); 
+    t = t';
+    x = x';
+    
+end
+
 
--- hw6/myfunc.m
+++ hw6/myfunc.m
@@ -1,42 +1,40 @@
 function xdot = myfunc(t, x)
-%x1 height, x2 velocity, x3 beta
+
 c = 10263;
 p_0 = 1220;
 rho = p_0*(exp(-x(1)/c));
 g = 9.8;
 d = (rho*(x(2)^2))/ (2*x(3));
 
-%xdot(1) height, xdot(2) velocity, xdot(3) beta
-% finding dx_hat(t) = f(x_hat, t)
+
+%loading the inital condition of covariance matrix P
+%since the inital variance guess is independent from each other
+%then the other covariance are zero.
 xdot = zeros(9,1);
-P(1,1)=x(4);
+P(1,1)=x(4);        %variance of height
 P(1,2)=x(5);
 P(1,3)=x(6);
 P(2,1) = x(7);
-P(2,2)=x(8);
+P(2,2)=x(8);        %variance of velocity
 P(2,3)=x(9);
 P(3,1)=x(10);
 p(3,2) = x(11);
-P(3,3)=x(12);
+P(3,3)=x(12);       %variance of beta
 
 
 
 %finding A(t) which is the jacobian
-
+%dv/dx                 dv/dv                dv/dbeta
+%d(d-g)/dx             d(d-g)/dv            d(d-g)/dbeta
+%d(sqrt(1000))/dx       d(sqrt(1000))/dv     d(sqrt(1000))/dbeta 
 %first row
 A11 = 0;
 A12 = 1;
 A13 = 0;
-
 %second row
 A21 = -(610*exp(-x(1)/10263)*(x(2)^2)) / (10263*x(3));
 A22 = (1220*exp(-x(1)/10263)*x(2) )/x(3);
 A23 = -(610*exp(-x(1)/10263)*(x(2)^2))/(x(3)^2);
-
-% %second row
-% A21 = -(610*exp((-x(1)/10263)*(x(2)^2))) / 10263*x(3);
-% A22 = (1220*exp((-x(1)/10263)*x(2)) )/x(3);
-% A23 = -(610*exp(-(x(1)/10263))*(x(2)^2))/(x(3)^2);
 
 %third row
 A31 = 0;
@@ -47,11 +45,13 @@ A = [A11 A12 A13; A21 A22 A23; A31 A32 A33];
 
 b = [0 0 0;0 0 0;0 0 1000];
 
+%calculativing the coveriance matrix
 Pdot=A*P+P*A'+b;
 
-xdot(1) = x(2) ;
-xdot(2) = d - g;
-xdot(3) = 0;
+%storing everything back to the return variable.
+xdot(1) = x(2) ;    %height
+xdot(2) = d - g;    %velociy
+xdot(3) = 0;        %beta
 
 xdot(4)=Pdot(1,1);
 xdot(5)=Pdot(1,2);
