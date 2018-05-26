c2d624640b8270b4cc9cb1953cc06c0a3aeb03bc
Wuyuan <wuyuan@pureengineering.com>
2018-05-26 11:54:40 -0700

    prediction step done, moving on to update

--- hw6/hw6.m
+++ hw6/hw6.m
@@ -4,12 +4,32 @@ clc
 
 %Wuyuan Chen
 %
-init_value = [10000 -500 6*(10^7) 50 0 0 0 200 0 0 0 2*(10^12)];
+x_hat(:, 1) = [10000; -500; 6*(10^7)];
+P_matrix(:, 1) = [50; 0; 0; 0; 200; 0; 0; 0; 2*(10^12)];
+
+init_value = [10000; -500; 6*(10^7); 50; 0; 0; 0; 200; 0; 0; 0; 2*(10^12)];
+
 for k=1:1:10
+    %-------------------Step 1 Prediction ----------------------%
     %using ode45 to solve for x_dot_hat(t) and A(t) in the prediction step 
     [t, x] = ode45('myfunc', [0 1], init_value); 
-    t = t';
-    x = x';
+    t = t';                             %turn it into a col vector
+    x = x';                             %turn it into a col vector
+    %getting the last column of values in each ode45 run
+    lastX = x(:,end);
+    x_hat(:, k) = [lastX(1); lastX(2); lastX(3)];
+    P_matrix(:, k) = [lastX(4); lastX(5); lastX(6)
+                      lastX(7); lastX(8); lastX(9)
+                      lastX(10); lastX(11); lastX(12)];
+    
+         
+    
+    
+    
+   
+    
+    
+    
     
 end
 
