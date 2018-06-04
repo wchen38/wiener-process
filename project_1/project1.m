7ea51e0fe120dda60424c7c7b2b2a7f79ae51d0e
Wuyuan <wuyuan@pureengineering.com>
2018-06-03 17:54:58 -0700

    minior changes on EKF, estimation is not correct, moving on to 2nd order for now

--- project_1/project1.m
+++ project_1/project1.m
@@ -16,51 +16,60 @@ y0 = yAxis(3);
 v0 = (xAxis(2) - xAxis(1)) / dt;
 theta0 = headingAngle(1);
 X = [x0; y0; v0; theta0];
+xRec(:, 1) = X;
 vRec = X(3);
 thetaRec = X(4);
 
 %inital guess of P
-varX0 = 1.3^2;
-varY0 = 1.3^2;
-varV0 = 2*varX0 / dt;
+varX0 = (1.3^2 +1.3^2+1.3^2)/3;
+varY0 = (1.3^2 +1.3^2+1.3^2)/3;
+varV0 = varX0 / dt;
 varTheta0 = (5*pi/180)^2;
 
 P = [varX0  0    0    0; 
       0   varY0  0    0;
       0     0  varV0  0;
       0     0    0 varTheta0];
-  
+
 P11Rec = P(1,1);
 p22Rec = P(2,2);
 P33Rec = P(3,3);
 P44Rec = P(4,4);
 
-sigmaV = 0.1;
-sigmaTheta = 0.2;
+P_Rec(:, 1) = [P11Rec; p22Rec; P33Rec; P44Rec];
 
-gamma = [0 0; 0 0; sigmaV*dt 0; 0 sigmaTheta*dt];
+sigmaV = 2;
+sigmaTheta = 2;
+
+gamma = [0 0; 0 0; sigmaV*sqrt(dt) 0; 0 sigmaTheta*sqrt(dt)];
 Q = [1 0; 0 1];
 
 %-----------------Prediction step---------------
-
-for k=1:length(xAxis)
-    X = X + [vRec*cos(thetaRec)*dt; vRec*sin(thetaRec)*dt; 0; 0];
+index = 2;
+for k=2:length(xAxis)
+    X = X + [vRec*cos(headingAngle(k))*dt; vRec*sin(headingAngle(k))*dt; 0; 0];
     
-    phi = [1    0   dt*cos(thetaRec) -vRec*dt*sin(thetaRec); 
-           0    1   dt*sin(thetaRec)  vRec*dt*cos(thetaRec);
-           0    0           1                  1;
+    phi = [1    0   dt*cos(headingAngle(k)) -vRec*dt*sin(headingAngle(k)); 
+           0    1   dt*sin(headingAngle(k))  vRec*dt*cos(headingAngle(k));
+           0    0           1                  0;
            0    0           0                  1;];
 
     P = phi*P*phi' + (gamma*Q*gamma');
-    
-     
+   
+    P_Rec(:, index) = [P(1,1); P(2,2); P(3,3); P(4,4);];
+    index = index +1;
 %-----------Update step -----------------------
 H = [1 0 0 0; 0 1 0 0];
 R = [1.3^2 0; 0 1.3^2];
-Z = [1 0 0 0; 0 1 0 0] * X + [1.3; 1.3];
-K = P*H'*inv(H*P*H' + R);
+Z = [1 0 0 0; 0 1 0 0] * X;
+K = (P*H')/(H*P*H' + R);
 X = X + K*(position(k)' - Z);
 xRec(:, k) = X;
 P = (eye(4) - K*H)*P;
-
+  P_Rec(:, index) = [P(1,1); P(2,2); P(3,3); P(4,4);]; 
+  index = index +1;
 end
+plot(xAxis, yAxis); hold on
+plot(xRec(1,:),xRec(2,:))
+
+
