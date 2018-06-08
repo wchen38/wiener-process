31bb71531d0905eeb5905e847d03e8e6de3c7df2
Wuyuan <wuyuan@pureengineering.com>
2018-06-08 15:48:15 -0700

    part 1 no more NaN, plots okay

--- project_2/project2.m
+++ project_2/project2.m
@@ -41,14 +41,13 @@ R = diag(varR);
 xm0 = S.xm(44); ym0 = S.ym(44); zm0 = S.zm(44); vx0 = (S.xm(44)-S.xm(43))/dt0;
 vy0 = (S.ym(44)-S.ym(43))/dt0; vz0 = (S.zm(44)-S.zm(43))/dt0;
 yawm0 = S.yawm(44);
-pitch = S.pitch;
-roll = S.roll;
+pitch = S.pitch * (pi/180);
+roll = S.roll * (pi/180);
 
 Xm = [xm0; ym0; zm0; vx0; vy0; vz0; yawm0];
 xRec = Xm;
 varP0 = [0.01^2 0.01^2 0.01^2 2*0.01^2/(S.dtrec(44)^2) 2*0.01^2/S.dtrec(44)^2 2*0.01^2/S.dtrec(44)^2 (pi/180)^2];
 Pm = diag(varP0);
-
 for k=45:length(S.xm)
      dt = S.dtrec(k-1);
      Q = diag(varQ) * dt;
@@ -66,8 +65,8 @@ for k=45:length(S.xm)
           0 0 0 0 0 0 1];
     % df4 = 0;
      %df5 = 0;
-     df4 = c*g*S.dtrec(k-1)*(cosd(Xm(7))*sind(roll(k-1)) - sind(Xm(7))*cosd(roll(k-1))*sind(pitch(k-1)));
-     df5 = c*g*S.dtrec(k-1)*(cosd(Xm(7))*cosd(roll(k-1))*sind(pitch(k-1)) + sind(Xm(7))*sind(roll(k-1)));
+     df4 = c*g*S.dtrec(k-1)*(cos(Xm(7))*sin(roll(k-1)) - sin(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)));
+     df5 = c*g*S.dtrec(k-1)*(cos(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)) + sin(Xm(7))*sin(roll(k-1)));
      phi = [1 0 0 dt 0 0 0;
             0 1 0 0 dt 0 0;
             0 0 1 0 0 dt 0;
@@ -76,22 +75,24 @@ for k=45:length(S.xm)
             0 0 0 0 0 1 0;
             0 0 0 0 0 0 1 ];
     %-------------------Prediction step-------------------------
-    Lx = c*g*(sind(Xm(7))*sind(roll(k-1)) + cosd(Xm(7))*cosd(roll(k-1))*sind(pitch(k-1)))*dt;
+    Lx = c*g*(sin(Xm(7))*sin(roll(k-1)) + cos(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)))*dt;
     %Lx = 0;
-    Ly = c*g*(sind(Xm(7))*cosd(roll(k-1))*sind(pitch(k-1)) - cosd(Xm(7))*sind(roll(k-1)))*dt;
+    Ly = c*g*(sin(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)) - cos(Xm(7))*sin(roll(k-1)))*dt;
     %Ly = 0;
-    Lz = (c*g*cosd(roll(k-1))*cosd(pitch(k-1)) - g)*dt;
+    Lz = (c*g*cos(roll(k-1))*cos(pitch(k-1)) - g)*dt;
     %Lz = 0;
    
-    Xm = Xm + [Xm(4)*dt; Xm(5)*dt; Xm(6)*dt; Lx; Ly; Lz; Xm(7)];
+    Xm = Xm + [Xm(4)*dt; Xm(5)*dt; Xm(6)*dt; Lx; Ly; Lz; 0];
     Pm = phi*Pm*phi' + gamma*Q*gamma';
     
     %-----------------Update step----------------------
+    Pm
     K = Pm*H'/(H*Pm*H' + R);
-    diff = [S.xm(k); S.ym(k); S.zm(k); S.yawm(k)] - [Xm(1); Xm(2); Xm(3); Xm(7)];
-    Xm = Xm + K*(diff);
+    res = [S.xm(k); S.ym(k); S.zm(k); S.yawm(k)] - [Xm(1); Xm(2); Xm(3); Xm(7)];
+    Xm = Xm + K*(res);
     xRec = [xRec Xm];
     Pm = (eye(7) - K*H)*Pm;
+    
 end 
 
 
