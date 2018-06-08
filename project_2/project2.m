7aabe37be94aac5dab23f12048bf5af7e3cd44fe
Wuyuan <wuyuan@pureengineering.com>
2018-06-07 18:21:58 -0700

    finished implementing everything but getting NaN

--- project_2/project2.m
+++ project_2/project2.m
@@ -1,4 +1,6 @@
 %Wuyuan Chen
+clear all
+clc
 S = load('data3D.mat');
 
 % plot3(S.xm, S.ym, S.zm); title('Evolution of position')
@@ -31,11 +33,12 @@ dt0 = S.dtrec(44);
 varQ = [1 1 1 .001];
 Q = diag(varQ) * dt0;
 
- 
-
+varR = [0.01^2, 0.01^2, 0.01^2, (pi/180)^2];
+R = diag(varR);
 
  
 %----------------------inital guess ----------------------------%
+xm0 = S.xm(44); ym0 = S.ym(44); zm0 = S.zm(44); vx0 = (S.xm(44)-S.xm(43))/dt0;
 vy0 = (S.ym(44)-S.ym(43))/dt0; vz0 = (S.zm(44)-S.zm(43))/dt0;
 yawm0 = S.yawm(44);
 pitch = S.pitch;
@@ -47,32 +50,49 @@ varP0 = [0.01^2 0.01^2 0.01^2 0.01^2/S.dtrec(44) 0.01^2/S.dtrec(44) 0.01^2/S.dtr
 Pm = diag(varP0);
 
 for k=45:length(S.xm)
-     dt = S.dtrec(k);
-     gamma = [0 0 0 0;
-         0 0 0 0;
-         0 0 0 0;
-         sigmaVx*sqrt(dt) 0 0 0;
-         0 sigmaVy*sqrt(dt) 0 0;
-         0 0 sigmaVz*sqrt(dt) 0;
-         0 0 0 sigmaYaw*sqrt(dt)];
+     dt = S.dtrec(k-1);
+%      gamma = [0 0 0 0;
+%          0 0 0 0;
+%          0 0 0 0;
+%          sigmaVx*sqrt(dt) 0 0 0;
+%          0 sigmaVy*sqrt(dt) 0 0;
+%          0 0 sigmaVz*sqrt(dt) 0;
+%          0 0 0 sigmaYaw*sqrt(dt)];
+  gamma = [0 0 0 0;
+           0 0 0 0;
+           0 0 0 0;
+           1 0 0 0;
+           0 1 0 0;
+           0 0 1 0;
+           0 0 0 1];
+     
+     H = [1 0 0 0 0 0 0;
+          0 1 0 0 0 0 0;
+          0 0 1 0 0 0 0;
+          0 0 0 0 0 0 1];
      
      df4 = c*g*S.dtrec(k-1)*(cos(Xm(7))*sin(roll(k-1)) - sin(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)));
      df5 = c*g*S.dtrec(k-1)*(cos(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)) + sin(Xm(7))*sin(roll(k-1)));
      phi = [1 0 0 dt 0 0 0;
-        0 1 0 0 dt 0 0;
-        0 0 1 0 0 dt 0;
-        0 0 0 1 0 0 df4;
-        0 0 0 0 1 0 df5;
-        0 0 0 0 0 1 0;
-        0 0 0 0 0 0 1;
-        ];
+            0 1 0 0 dt 0 0;
+            0 0 1 0 0 dt 0;
+            0 0 0 1 0 0 df4;
+            0 0 0 0 1 0 df5;
+            0 0 0 0 0 1 0;
+            0 0 0 0 0 0 1 ];
     %-------------------Prediction step-------------------------
-    Lx = c*g*(sin(Xm(7))*sin(roll(k)) + cos(Xm(7))*cos(roll(k))*sin(pitch(k)))*dt;
-    Ly = c*g*(sin(Xm(7))*cos(roll(k))*sin(pitch(k)) - cos(Xm(7))*sin(roll(k)))*dt;
-    Lz = (c*g*cos(roll(k))*cos(pitch(k)) - g)*dt;
+    Lx = c*g*(sin(Xm(7))*sin(roll(k-1)) + cos(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)))*dt;
+    Ly = c*g*(sin(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)) - cos(Xm(7))*sin(roll(k-1)))*dt;
+    Lz = (c*g*cos(roll(k-1))*cos(pitch(k-1)) - g)*dt;
    
     Xm = Xm + [Xm(4)*dt; Xm(5)*dt; Xm(6)*dt; Lx; Ly; Lz; Xm(7)];
     Pm = phi*Pm*phi' + gamma*Q*gamma';
+    
+    %-----------------Update step----------------------
+    K = Pm*H'/(H*Pm*H' + R);
+    diff = [S.xm(k); S.ym(k); S.zm(k); S.yawm(k)] - [Xm(1); Xm(2); Xm(3); Xm(7)];
+    Xm = Xm + K*(diff);
+    Pm = (eye(7) - K*H)*Pm;
 end 
 
 
