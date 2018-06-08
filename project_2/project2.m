40f7dfa73aa88f72d43ee6996cfd7f4dacc566dc
Wuyuan <wuyuan@pureengineering.com>
2018-06-08 14:59:53 -0700

    change sin to sind

--- project_2/project2.m
+++ project_2/project2.m
@@ -21,17 +21,17 @@ S = load('data3D.mat');
 % figure
 % plot(S.zm); title('Evolution of z')
 
-sigmaVx = 1;
-sigmaVy = 1;
-sigmaVz = 1;
-sigmaYaw = 1;
+sigmaVx = 2*0.01^2/(S.dtrec(44)^2);
+sigmaVy = 2*0.01^2/(S.dtrec(44)^2);
+sigmaVz = 2*0.01^2/(S.dtrec(44)^2);
+sigmaYaw = (pi/180)^2;
 
  g = 9.8;
  c = 1;
 
 dt0 = S.dtrec(44);
 varQ = [1 1 1 .001];
-Q = diag(varQ) * dt0;
+
 
 varR = [0.01^2, 0.01^2, 0.01^2, (pi/180)^2];
 R = diag(varR);
@@ -45,34 +45,29 @@ pitch = S.pitch;
 roll = S.roll;
 
 Xm = [xm0; ym0; zm0; vx0; vy0; vz0; yawm0];
-
-varP0 = [0.01^2 0.01^2 0.01^2 0.01^2/S.dtrec(44) 0.01^2/S.dtrec(44) 0.01^2/S.dtrec(44) (pi/180)^2];
+xRec = Xm;
+varP0 = [0.01^2 0.01^2 0.01^2 2*0.01^2/(S.dtrec(44)^2) 2*0.01^2/S.dtrec(44)^2 2*0.01^2/S.dtrec(44)^2 (pi/180)^2];
 Pm = diag(varP0);
 
 for k=45:length(S.xm)
      dt = S.dtrec(k-1);
-%      gamma = [0 0 0 0;
-%          0 0 0 0;
-%          0 0 0 0;
-%          sigmaVx*sqrt(dt) 0 0 0;
-%          0 sigmaVy*sqrt(dt) 0 0;
-%          0 0 sigmaVz*sqrt(dt) 0;
-%          0 0 0 sigmaYaw*sqrt(dt)];
-  gamma = [0 0 0 0;
+     Q = diag(varQ) * dt;
+     gamma = [0 0 0 0;
            0 0 0 0;
            0 0 0 0;
-           1 0 0 0;
-           0 1 0 0;
-           0 0 1 0;
-           0 0 0 1];
+           sigmaVx 0 0 0;
+           0 sigmaVy 0 0;
+           0 0 sigmaVz 0;
+           0 0 0 sigmaYaw];
      
      H = [1 0 0 0 0 0 0;
           0 1 0 0 0 0 0;
           0 0 1 0 0 0 0;
           0 0 0 0 0 0 1];
-     
-     df4 = c*g*S.dtrec(k-1)*(cos(Xm(7))*sin(roll(k-1)) - sin(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)));
-     df5 = c*g*S.dtrec(k-1)*(cos(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)) + sin(Xm(7))*sin(roll(k-1)));
+    % df4 = 0;
+     %df5 = 0;
+     df4 = c*g*S.dtrec(k-1)*(cosd(Xm(7))*sind(roll(k-1)) - sind(Xm(7))*cosd(roll(k-1))*sind(pitch(k-1)));
+     df5 = c*g*S.dtrec(k-1)*(cosd(Xm(7))*cosd(roll(k-1))*sind(pitch(k-1)) + sind(Xm(7))*sind(roll(k-1)));
      phi = [1 0 0 dt 0 0 0;
             0 1 0 0 dt 0 0;
             0 0 1 0 0 dt 0;
@@ -81,9 +76,12 @@ for k=45:length(S.xm)
             0 0 0 0 0 1 0;
             0 0 0 0 0 0 1 ];
     %-------------------Prediction step-------------------------
-    Lx = c*g*(sin(Xm(7))*sin(roll(k-1)) + cos(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)))*dt;
-    Ly = c*g*(sin(Xm(7))*cos(roll(k-1))*sin(pitch(k-1)) - cos(Xm(7))*sin(roll(k-1)))*dt;
-    Lz = (c*g*cos(roll(k-1))*cos(pitch(k-1)) - g)*dt;
+    Lx = c*g*(sind(Xm(7))*sind(roll(k-1)) + cosd(Xm(7))*cosd(roll(k-1))*sind(pitch(k-1)))*dt;
+    %Lx = 0;
+    Ly = c*g*(sind(Xm(7))*cosd(roll(k-1))*sind(pitch(k-1)) - cosd(Xm(7))*sind(roll(k-1)))*dt;
+    %Ly = 0;
+    Lz = (c*g*cosd(roll(k-1))*cosd(pitch(k-1)) - g)*dt;
+    %Lz = 0;
    
     Xm = Xm + [Xm(4)*dt; Xm(5)*dt; Xm(6)*dt; Lx; Ly; Lz; Xm(7)];
     Pm = phi*Pm*phi' + gamma*Q*gamma';
@@ -92,6 +90,7 @@ for k=45:length(S.xm)
     K = Pm*H'/(H*Pm*H' + R);
     diff = [S.xm(k); S.ym(k); S.zm(k); S.yawm(k)] - [Xm(1); Xm(2); Xm(3); Xm(7)];
     Xm = Xm + K*(diff);
+    xRec = [xRec Xm];
     Pm = (eye(7) - K*H)*Pm;
 end 
 
